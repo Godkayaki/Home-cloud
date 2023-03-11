a7f3b8ddd3b27e7db4c3453e4320552d503fe15f
Godkayaki <daniplcm@gmail.com>
2023-03-11 20:32:11 +0100

    fix form

--- main.py
+++ main.py
@@ -23,12 +23,10 @@ class UploadFileForm(FlaskForm):
 @app.route('/home', methods=['GET',"POST"])
 def home():
     form = UploadFileForm()
-    
     if form.validate_on_submit():
         file = form.file.data # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
         return "File has been uploaded."
-    
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':

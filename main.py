46da6a0ff33a0a917a84cd6a7dd2255ceca77926
Godkayaki <daniplcm@gmail.com>
2023-03-12 18:51:15 +0100

    update multiple files

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import MultipleFileField, FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
+    file = MultipleFileField("File(s) Upload", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
 @app.route('/', methods=['GET',"POST"])
@@ -24,10 +24,22 @@ class UploadFileForm(FlaskForm):
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
+        files = form.file.data # First grab the file
+        return_value = ""
+        #print(files)
+        
+        for file in files:
+            file_path = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "/" + str(app.config['UPLOAD_FOLDER']) + "/" + str(file.filename)
+            
+            if os.path.isfile(file_path): 
+                return_value = return_value + file_path + " -> file already exists.\n"
+            else:
+                file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+                return_value = return_value + file_path + " -> file uploaded.\n"
+        
+        return return_value
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True, port=5001)
+    app.run()

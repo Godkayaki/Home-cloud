60d762464cb4880bac55bd258fda37a7a40bcb07
Godkayaki <daniplcm@gmail.com>
2023-03-22 21:40:36 +0100

    navigation + download

--- main.py
+++ main.py
@@ -4,29 +4,46 @@
 # 
 # Home-cloud
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_wtf import FlaskForm
 from wtforms import MultipleFileField, FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 
+destination_path = 'static/files'
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOAD_FOLDER'] = destination_path
 
 class UploadFileForm(FlaskForm):
     file = MultipleFileField("File(s) Upload", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
-def home():
+@app.route('/', methods=['GET',"POST"], defaults={'req_path': ''})
+@app.route('/<path:req_path>')
+def home(req_path):
+    
+    current_path = ""
+    if destination_path.endswith("/") or req_path.startswith("/"):
+        current_path = destination_path + req_path
+    else:
+        current_path = destination_path + "/" + req_path
+
+    #if path clicked 
+    if os.path.isfile(current_path):
+        return send_file(current_path, as_attachment=True)
+    
     form = UploadFileForm()
+    file_list = os.listdir(current_path)
+    file_list.insert(0,".")
+    print(file_list)
+
+    #manage uploading files
     if form.validate_on_submit():
         files = form.file.data # First grab the file
         return_value = ""
-        #print(files)
         
         for file in files:
             file_path = os.path.join(os.path.abspath(os.path.dirname(__file__))) + "/" + str(app.config['UPLOAD_FOLDER']) + "/" + str(file.filename)
@@ -38,7 +55,7 @@ def home():
                 return_value = return_value + file_path + " -> file uploaded.\n"
         
         return return_value
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, files=file_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
